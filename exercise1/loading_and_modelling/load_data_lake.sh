049b977f666f867bf4364a214ac4d063ffce8590
goku8765 <goten@gmail.com>
2017-07-04 16:23:04 -0500

    1st

--- exercise1/loading_and_modelling/load_data_lake.sh
+++ exercise1/loading_and_modelling/load_data_lake.sh
@@ -1,8 +1,5 @@
-##
-
-echo "Hello World!"
 tail -n +2 Hospital\ General\ Information.csv > hospitals.csv
-tail -n +2 Timely\ and\ Effective\ Care - Hospital.csv > effective_care.csv
-tail -n +2 Readmissions\ and\ Deaths - Hospital.csv > readmissions.csv
+tail -n +2 Timely\ and\ Effective\ Care\ -\ Hospital.csv > effective_care.csv
+tail -n +2 Readmissions\ and\ Deaths\ -\ Hospital.csv > readmissions.csv
 tail -n +2 Measure\ Dates.csv > Measures.csv
 tail -n +2 hvbp_hcahps_05_28_2015.csv > surveys_responses.csv
