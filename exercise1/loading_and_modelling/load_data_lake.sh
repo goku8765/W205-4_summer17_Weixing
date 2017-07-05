d4ffece6ddf3c49a0b0d165e29134926dc61c996
goku8765 <goten@gmail.com>
2017-07-04 23:12:07 -0500

    11

--- exercise1/loading_and_modelling/load_data_lake.sh
+++ exercise1/loading_and_modelling/load_data_lake.sh
@@ -1,5 +1,5 @@
 hdfs dfs -put hospitals.csv /user/w205/hospital_compare
 hdfs dfs -put effective_care.csv /user/w205/hospital_compare
 hdfs dfs -put readmissions.csv /user/w205/hospital_compare
-hdfs dfs -put survey_responses.csv /user/w205/hospital_compare
+hdfs dfs -put surveys_responses.csv /user/w205/hospital_compare
 hdfs dfs -put measures.csv /user/w205/hospital_compare
